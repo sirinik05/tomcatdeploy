b64950a56ced723a9cf1ab59085b49700f5ee2ca
sirinik05 <48499606+sirinik05@users.noreply.github.com>
2019-04-10 22:06:03 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,44 @@
+variable "key_name" {
+  default = "siriAWS.pub"
+}
+
+variable "aws_access_key" {}
+
+variable "aws_secret_key" {}
+
+variable "private_key_path" {
+  default = "/opt/key/siriAWS"
+}
+
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_type" {
+  default = "ami-03b5146883373319b"
+}
+
+variable "my_default_port" {
+  default = "22"variable "my_cidr_blocks" {
+  default = ["69.119.126.1/32"]
+}
+
+variable "my_pub_port" {
+  default = "0"
+}
+
+variable "my_pub_protocol" {
+  default = "-1"
+}
+
+variable "my_tom_port" {
+  default = "8080"
+}
+
+variable "my_protocol" {
+  default = "tcp"
+}
